de964717d91a752a270d7788830d59741b5d1f0c
Stromy08 <fajnygoscsteve@gmail.com>
2023-12-03 16:51:21 +0000

    0.0.1.23
    fixed wheels

--- GAme/Assets/scripts/SimpleCarController.cs
+++ GAme/Assets/scripts/SimpleCarController.cs
@@ -1,88 +1,92 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class SimpleCarController : MonoBehaviour {
+public class SimpleCarController : MonoBehaviour
+{
     public List<AxleInfo> axleInfos;
     public float maxMotorTorque;
     public float maxSteeringAngle;
     public Image speedometer;
     public TMP_Text speedText;
     public float maxSpeed = 50f;
+
     public gameManager gameManager;
-    public  bool isFlipped;
-    // search how to read a transform value in unity. make it so that if X>50 or Z>30: is flipped = true; if (isFlipped=true) transform rotation (0, Y, 0)
 
 
-   public void FixedUpdate()
-   {
+    void Start()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
         if (gameManager.IsDriving)
         {
             float motor = maxMotorTorque * Input.GetAxis("Vertical");
             float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-            
-            foreach (AxleInfo axleInfo in axleInfos) {
-                if (axleInfo.steering) {
+
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                if (axleInfo.steering)
+                {
                     axleInfo.leftWheel.steerAngle = steering;
                     axleInfo.rightWheel.steerAngle = steering;
                 }
-                if (axleInfo.motor) {
+                if (axleInfo.motor)
+                {
                     axleInfo.leftWheel.motorTorque = motor;
                     axleInfo.rightWheel.motorTorque = motor;
                 }
-                if (Input.GetKey(KeyCode.Space) && axleInfo.handBrake) { //currently not working. might remove//
-                     axleInfo.leftWheel.motorTorque = 0;
-                     axleInfo.rightWheel.motorTorque = 0;
+                if (Input.GetKey(KeyCode.Space) && axleInfo.handBrake)
+                { //currently not working. might remove//
+                    axleInfo.leftWheel.motorTorque = 0;
+                    axleInfo.rightWheel.motorTorque = 0;
+
                 }
                 ApplyLocalPositionToVisuals(axleInfo.leftWheel);
                 ApplyLocalPositionToVisuals(axleInfo.rightWheel);
             }
         }
-   }
+    }
 
-   public void ApplyLocalPositionToVisuals(WheelCollider collider)
-   {
-       if (collider.transform.childCount == 0) {
-           return;
-       }
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        Transform parent = collider.transform.parent;
+        Transform visualWheel = parent.GetChild(0);
 
-       Transform visualWheel = collider.transform.GetChild(0);
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
 
-       Vector3 position;
-       Quaternion rotation;
-       collider.GetWorldPose(out position, out rotation);
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
 
-       visualWheel.transform.position = position;
-       visualWheel.transform.rotation = rotation;
-
-       visualWheel.transform.Rotate(0, 0, 90);
-   }
 
     void Update()
     {
         UpdateSpeedometer();
     }
 
-
-
-
-   private void UpdateSpeedometer()
-   {
-       float speed = GetComponent<Rigidbody>().velocity.magnitude;
-       float fillPercent = speed / maxSpeed;
-       speedometer.fillAmount = fillPercent;
-       speedText.text = ((int)speed).ToString() + " km/h";
-   }
+    private void UpdateSpeedometer()
+    {
+        float speed = GetComponent<Rigidbody>().velocity.magnitude;
+        float fillPercent = speed / maxSpeed;
+        speedometer.fillAmount = fillPercent;
+        speedText.text = ((int)speed).ToString() + " km/h";
+    }
 
 }
 
 [System.Serializable]
-public class AxleInfo {
-   public WheelCollider leftWheel;
-   public WheelCollider rightWheel;
-   public bool motor; 
-   public bool steering; 
-   public bool handBrake;
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+    public bool handBrake;
 }
