69b4324d06a247a991998ed50627f84a8a678b8d
Stromy08 <fajnygoscsteve@gmail.com>
2023-12-13 18:46:15 +0000

    0.1.2.0
    added sprinting

--- GAme/Assets/scripts/playerController.cs
+++ GAme/Assets/scripts/playerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player behaviour Settings")]
     public float speed = 4f;
+    public float sprintSpeed = 8f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
     Vector3 velocity;
+    bool isSprinting;
 
     [Header("Define objects ")]
     public Transform cam;
@@ -184,6 +186,19 @@ public class PlayerController : MonoBehaviour
             animator.SetTrigger("Jump");
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+            animator.SetBool("IsWalking", direction.magnitude >= 0.1f);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isSprinting = false;
+        }
+        float speedToUse = isSprinting ? sprintSpeed : speed;
+        // set animatior sprinting
+        animator.SetBool("IsSprinting", isSprinting);
+
         velocity.y += gravity * Time.deltaTime;
 
         if (direction.magnitude >= 0.1f)
@@ -193,7 +208,7 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            controller.Move(moveDirection.normalized * speedToUse * Time.deltaTime);
         }
 
         controller.Move(velocity * Time.deltaTime);
